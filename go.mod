5703066182d24b4bb1f28acf9def5c4ddccace94
abgr <tbpmtbpm@gmail.com>
2021-08-18 01:41:53 +0430

    make it lock-free

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/itsabgr/go-q
 
 go 1.16
 
-require github.com/itsabgr/go-handy v0.0.0-20210816165329-4cd57d210275
+require (
+	github.com/itsabgr/atomic2 v0.0.0-20210817205133-e2171f145192
+	github.com/itsabgr/go-handy v0.0.0-20210816165329-4cd57d210275
+)
--- q.go
+++ q.go
@@ -1,87 +1,110 @@
 package q
 
 import (
+	"github.com/itsabgr/atomic2"
 	. "github.com/itsabgr/go-handy"
-	"sync"
+	"unsafe"
 )
 
 type item struct {
 	value Any
-	next  *item
+	next  atomic2.Ptr
+}
+
+func (r *item) setNext(another *item) bool {
+	return r.next.CAS(nil, unsafe.Pointer(another))
+}
+func (r *item) getNext() *item {
+	return (*item)(r.next.Get())
+}
+func (r *item) tail() *item {
+	tail := r
+	for {
+		next := tail.getNext()
+		if next == nil {
+			return tail
+		}
+		tail = next
+	}
+}
+func (r *item) isTail() bool {
+	return r.next.Get() == nil
 }
 
 //Q is a thread-safe Queue
-type Q struct {
-	mutex sync.Mutex
-	head  *item
-	tail  *item
+type Q item
+
+//Reset skip all items in queue
+func (r *Q) Reset() {
+	r.next.Set(nil)
 }
 
 //Push add value to tail of queue
 func (r *Q) Push(value interface{}) {
-	r.mutex.Lock()
-	defer r.mutex.Unlock()
-	r.push(value)
-}
-
-//Reset skip all items in queue
-func (r *Q) Reset() {
-	r.mutex.Lock()
-	defer r.mutex.Unlock()
-	r.head = nil
-	r.tail = nil
-}
-
-func (r *Q) push(value interface{}) {
-	pre := r.tail
-	r.tail = &item{
+	newItem := &item{
 		value: value,
 	}
-	pre.next = r.tail
+	for {
+		tail := r.getTail()
+		if tail.setNext(newItem) {
+			return
+		}
+	}
+}
+func (r *Q) getTail() *item {
+	tail := r.asItem()
+	for {
+		if tail.isTail() {
+			return tail
+		}
+		tail = tail.getNext()
+	}
 }
 
 //Peek returns head item of queue without removing it.
 //if no item found returns nil,false
-func (r *Q) Peek() (value interface{}, found bool) {
-	r.mutex.Lock()
-	defer r.mutex.Unlock()
-	return r.Peek()
-}
-func (r *Q) peek() (interface{}, bool) {
-	if r.head == nil {
+func (r *Q) Peek() (interface{}, bool) {
+	next := r.asItem().getNext()
+	if next == nil {
 		return nil, false
 	}
-	value := r.head.value
-	return value, true
+	return next.value, true
 }
 
 //Skip removes head item of queue.
 //returns false if no item found
 func (r *Q) Skip() bool {
-	r.mutex.Lock()
-	defer r.mutex.Unlock()
-	return r.skip()
+	for {
+		head := r.getHead()
+		if head == nil {
+			return false
+		}
+		if r.casHead(head, head.getNext()) {
+			return true
+		}
+	}
 }
-func (r *Q) skip() bool {
-	if r.head == nil {
-		return false
-	}
-	r.head = r.head.next
-	if r.head == nil {
-		r.tail = nil
-	}
-	return true
+func (r *Q) getHead() *item {
+	return r.asItem().getNext()
+}
+func (r *Q) casHead(test *item, new *item) bool {
+	return r.next.CAS(unsafe.Pointer(test), unsafe.Pointer(new))
+}
+
+func (r *Q) asItem() *item {
+	return (*item)(r)
 }
 
 //Pull returns head item of queue and  remove it.
 //if no item found returns nil,false
-func (r *Q) Pull() (value interface{}, found bool) {
-	r.mutex.Lock()
-	defer r.mutex.Unlock()
-	return r.pull()
-}
-func (r *Q) pull() (interface{}, bool) {
-	value, ok := r.peek()
-	r.skip()
-	return value, ok
+func (r *Q) Pull() (interface{}, bool) {
+	for {
+		head := r.getHead()
+		if head == nil {
+			return nil, false
+		}
+		if r.casHead(head, head.getNext()) {
+			return head.value, true
+		}
+	}
 }
