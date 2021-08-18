b65b7765e2b586a64781fd338feceed6c2ce949f
abgr <tbpmtbpm@gmail.com>
2021-08-18 15:06:36 +0430

    impr tests

--- q_test.go
+++ q_test.go
@@ -1,10 +1,55 @@
 package q
 
 import (
+	"github.com/itsabgr/atomic2"
 	"github.com/itsabgr/go-handy"
+	"runtime"
+	"sync"
 	"testing"
 )
 
+func TestQ_ThreadSafety(t *testing.T) {
+	if runtime.NumCPU() <= 1 {
+		t.Fatal("can not test thread-safety on this environments")
+	}
+	total := atomic2.Uintptr(0)
+	q := Q{}
+	wg := sync.WaitGroup{}
+	count := uint(9999)
+	goroutines := uint(runtime.NumCPU())
+	for _ = range handy.N(goroutines) {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for range handy.N(count) {
+				q.Push(total.Add(1))
+			}
+		}()
+	}
+	wg.Wait()
+	n := total.Get()
+	if uint(n) != count*goroutines {
+		t.Fatalf("expected %d got %d", count*goroutines, n)
+	}
+	for _ = range handy.N(goroutines) {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for range handy.N(count) {
+				_, ok := q.Pull()
+				if !ok {
+					break
+				}
+				total.Sub(1)
+			}
+		}()
+	}
+	wg.Wait()
+	n = total.Get()
+	if n != 0 {
+		t.Fatalf("expcted 0 got %d", int(n))
+	}
+}
 func TestQ_Overall(t *testing.T) {
 	q := Q{}
 	for i := range handy.N(10) {
